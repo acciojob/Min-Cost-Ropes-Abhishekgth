450d4794dba9162b14479896203a372f02ae09be
Abhishekgth <128219073+Abhishekgth@users.noreply.github.com>
2024-03-20 12:36:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function mincost(arr)
+/*function mincost(arr)
 { 
 //write your code here
 // return the min cost
-if (arr.length === 0) return 0;
+/*if (arr.length === 0) return 0;
   arr.sort((a, b) => a - b);
 
   let cost = 0;
@@ -18,7 +18,19 @@ if (arr.length === 0) return 0;
   }
 
   return cost;
+	
   
+}*/
+function mincost(arr){ 
+  let sum=0;
+while(arr.length>1){
+	arr.sort((a,b)=>a-b);
+	let a = arr.shift();
+	let b = arr.shift();
+	sum+=a+b;
+	arr.push(a+b);
+}
+	return sum;
 }
 
 module.exports=mincost;
